e9bf5790606d7b86c2edcae6b8c9a67bd11b8066
vhincechristian <mvcdcugot@tip.edu.ph>
2022-09-19 22:33:04 +0800

    dasdad

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,72 +1,38 @@
 <template>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header">Register</div>
-            <div class="card-body">
+  <v-card class="ma-5 mx-auto mx-auto text-black" max-width="600">
+           <v-card-title>Register</v-card-title>
+            
               <div v-if="error" class="alert alert-danger">{{error}}</div>
-              <form action="#" @submit.prevent="Register">
-                <div class="form-group row">
-                  <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
-  
-                  <div class="col-md-6">
-                    <input
-                      id="name"
-                      type="name"
-                      class="form-control"
-                      name="name"
-                      required
-                      autofocus
-                      v-model="name"
-                    />
-                  </div>
-                </div>
-  
-                <div class="form-group row">
-                  <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-  
-                  <div class="col-md-6">
-                    <input
-                      id="email"
-                      type="email"
-                      class="form-control"
-                      name="email"
-        
-                      required
-                      autofocus
-                      v-model="email"
-                    />
-                  </div>
-                </div>
-  
-                <div class="form-group row">
-                  <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-  
-                  <div class="col-md-6">
-                    <input
-                      id="password"
-                      type="password"
-                      class="form-control"
-                      name="password"
-                      required
-                      v-model="password"
-                    />
-                  </div>
-                </div>
-  
-                <div class="form-group row mb-0">
-                  <div class="col-md-8 offset-md-4">
+             
+              <v-text-field 
+              label="Name" 
+              id="name" 
+              type="text" 
+              name="name" 
+              required v-model="name" 
+              class="mt-3"></v-text-field>
+
+              <v-text-field  
+              label="Email" 
+              id="email" 
+              type="email" 
+              name="email" 
+              required v-model="email" 
+              class="mt-3"></v-text-field>
+
+              <v-text-field 
+              label="Password" 
+              id="password" 
+              type="password" 
+              name="password" 
+              required v-model="password" 
+              class="mt-3"></v-text-field>
+
+              <v-btn color="#90EE90" class="primary" @click="Register">
                     <button type="submit" class="btn btn-primary">Register</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </template>
+              </v-btn>
+  </v-card>
+</template>
   
   
   <script>
